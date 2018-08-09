6e75ab72cdad4f5937545cc740744d23e71239c4
DhrubajyotiDebnathcts <Dhrubajyoti.Debnath3@cognizant.com>
2018-08-09 12:28:21 +0000

    adding smoke test script for login check

--- SmokeTesting/login_check.sh
+++ SmokeTesting/login_check.sh
@@ -10,7 +10,23 @@ echo $RESPONSE
 
 
 if [[ $RESPONSE = *"\"status\":\"SUCCESS\""* ]]; then
-  echo "Login is working with $TEST" > login.log
+  echo "Login is working with $TEST" >> login.log
 else
-  echo "Login is not working TEST" > login.log
+  echo "Login is not working TEST" >> login.log
 fi
+mq_test="curl -X GET -u iSight:iSight http://localhost:15672/api/queues"
+queue_limit=1
+RESPONSE=`$mq_test`
+
+for row in $(echo "${RESPONSE}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+   queue_size=$(_jq '.messages')
+   if [[ $queue_size -gt $queue_limit ]]; then
+       echo $(_jq '.name') >> login.log
+       echo $queue_size >> login.log
+       echo "This is more than expected" >> login.log
+   fi
+done
